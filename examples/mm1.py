b8b17a20a0ffa48497d4078ff78a1ca09a1014fa
Ryan J. O'Neil <roneil@mitre.org>
2012-07-02 14:09:24 -0400

    explanatory comments

--- examples/mm1.py
+++ examples/mm1.py
@@ -2,12 +2,15 @@
 
 import chute
 
+# A process is anything that is callable. And a resource can be anything
+# that is hashable by Python, so really anything at all:
 @chute.process(chute.dist.exponential(1))
 def customer():
     yield chute.request, 'server 1'
     yield chute.hold, chute.dist.exponential(0.25)
     yield chute.release, 'server 1'
 
+# Here is an equivalent process, from the perspective of chute:
 @chute.process(chute.dist.exponential(1))
 class Customer(object):
     NUM = 1
@@ -24,6 +27,8 @@ class Customer(object):
         yield chute.hold, chute.dist.exponential(0.25)
         yield chute.release, 'server 1'
 
+# Simulator objects can be created manually as below, or will available
+# with normal defaults using the chute command:
 if __name__ == '__main__':
     s = chute.Simulator()
     s.run(10)
--- src/chute/dist.py
+++ src/chute/dist.py
@@ -1,4 +1,13 @@
 import random
 
 def exponential(l):
+    '''An exponential random variate with lambda=l.'''
     return lambda: random.expovariate(l)
+
+def triangular(low=0.0, high=1.0, mode=None):
+    '''A triangular random variate with parameters a, b, mode.'''
+    return lambda: low, high, mode=None)
+
+def uniform(a=0.0, b=1.0):
+    '''A uniform random variate from a to b.'''
+    return lambda: random.uniform(a, b)
