86ce20c2df210a1e83910768949ad0b2effa0fd6
Ryan J. O'Neil <roneil@mitre.org>
2012-09-04 10:55:22 -0400

    fixed triangular dist

--- src/chute/dist.py
+++ src/chute/dist.py
@@ -6,7 +6,7 @@ def exponential(l):
 
 def triangular(low=0.0, high=1.0, mode=None):
     '''A triangular random variate with parameters a, b, mode.'''
-    return lambda: low, high, mode=None)
+    return lambda: random.triangular(low, high, mode=None)
 
 def uniform(a=0.0, b=1.0):
     '''A uniform random variate from a to b.'''
